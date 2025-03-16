5bc25352c313751b18e2da38ad175dc1585fa01a
NapolDeveloper <loma1016@gmail.com>
2025-03-16 14:49:18 +0900

    chore: any 타입 사용 금지

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,10 @@ const eslintConfig = [
       // 선언은 했지만 사용되지 않은 변수
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
+
+      // any 타입 사용 금지
+      // any 타입 사용이 필요할 경우 해당 라인만 린트 비활성화 후 사용
+      '@typescript-eslint/no-explicit-any': 'error',
     },
   }),
 ];
