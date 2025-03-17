f8bca7eccecc58ec1ee267a8b9e1f7a9872cc0df
NapolDeveloper <loma1016@gmail.com>
2025-03-17 15:43:49 +0900

    feat: 브랜드관 레이아웃 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { Text } from 'components/ui/text';
+
+import BrandSection from './_components/brand-section';
 import MainBanner from './_components/main-banner';
 
 export default function Home() {
@@ -5,6 +8,13 @@ export default function Home() {
     <>
       <section className="px-16 py-10">
         <MainBanner />
+      </section>
+
+      <section className="px-16 py-10">
+        <Text as="h2" size="2xl" weight="bold" className="mb-4">
+          브랜드관
+        </Text>
+        <BrandSection />
       </section>
     </>
   );
