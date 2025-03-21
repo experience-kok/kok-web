c01a87e9879ee2dd169ae602a5ca536262cb5931
NapolDeveloper <loma1016@gmail.com>
2025-03-21 13:44:11 +0900

    feat: 카카오 로그인 버튼 구현

---
+++ src/app/login/_components/login-button.tsx
@@ -0,0 +1,8 @@
+import { Button } from 'components/ui/button';
+
+/**
+ * 로그인 버튼 컴포넌트
+ */
+export default function LoginButton() {
+  return <Button>로그인</Button>;
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,21 @@
+import { Input } from 'components/ui/input';
+import { Text } from 'components/ui/text';
+
+import KakaoLoginButton from './_components/kakao-login-button';
+import LoginButton from './_components/login-button';
+
 /**
  * 로그인 페이지
  */
 export default function LoginPage() {
-  return <>d</>;
+  return (
+    <section className="px-4">
+      <Text>체험콕</Text>
+      <Input />
+      <Input />
+      <LoginButton />
+
+      <KakaoLoginButton />
+    </section>
+  );
 }
