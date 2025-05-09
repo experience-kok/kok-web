04ab1aa209a29976eb2dffd584f5738f7a93a55f
NapolDeveloper <loma1016@gmail.com>
2025-05-09 09:22:27 +0900

    fix: 빌드 에러 수정

--- src/app/(service)/(mobile-env)/mypage/edit/page.tsx
+++ src/app/(service)/(mobile-env)/mypage/edit/page.tsx
@@ -11,7 +11,7 @@ export default function MyProfileEditPage() {
   return (
     <>
       <ErrorBoundary fallback={ErrorFallback}>
-        <Suspense>
+        <Suspense clientOnly>
           <MyProfileEditContainer />
         </Suspense>
       </ErrorBoundary>
